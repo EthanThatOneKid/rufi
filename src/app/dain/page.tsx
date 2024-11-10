c7885fb38e25b8ac6278b61f3a2c6b6b255477b1
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-11-10 09:19:00 -0800

    Connecting register endpoint

--- src/app/dain/page.tsx
+++ src/app/dain/page.tsx
@@ -1,17 +1,36 @@
-"use client";
+'use client';
 
-import { startAnyoneClient } from "@/lib/anon";
-import { useEffect, useState, Suspense, SetStateAction } from "react";
+import { startAnyoneClient } from '@/lib/anon';
+import { useEffect, useState, Suspense, SetStateAction } from 'react';
+
+// async function main() {
+//   const anon = new Anon();
+//   const anonSocksClient = new AnonSocksClient(anon);
+
+//   try {
+//     await anon.start();
+//     await new Promise((resolve) => setTimeout(resolve, 15000));
+
+//     const response = await anonSocksClient.get(
+//       'https://api.ipify.org?format=json'
+//     );
+//     console.log('Response:', response.data);
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     await anon.stop();
+//   }
+// }
 
 export function SignalDataView() {
   const [data, setData] = useState(null);
-  useEffect(() => {
-    startAnyoneClient().then(async (anon) => {
-      await anon
-        .request("/api/signals")
-        .then((data: SetStateAction<null>) => setData(data));
-    });
-  }, [startAnyoneClient]);
+  // useEffect(() => {
+  //   startAnyoneClient().then(async (anon) => {
+  //     await anon
+  //       .request('/api/signals')
+  //       .then((data: SetStateAction<null>) => setData(data));
+  //   });
+  // }, [startAnyoneClient]);
 
   if (!data) {
     return <div>Loading...</div>;
